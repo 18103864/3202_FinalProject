9df4b8426ef0807afce199da1a914c633673f994
sey <90585546+Celdinee@users.noreply.github.com>
2024-05-21 11:19:10 +0800

    Update navbar.js
    change website title

--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -15,7 +15,7 @@ export const Navbar = () => {
     return(
         <nav className="navbar navbar-expand-lg bg-dark border-bottom border-body" data-bs-theme="dark">
             <div className="container-fluid">
-                <a className="navbar-brand text-info" href="#">Navbar</a>
+                <a className="navbar-brand text-info" href="#">124movies</a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
                 </button>
