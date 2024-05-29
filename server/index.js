f2c73845b9399acaf35c7c028c611ade66d7938f
jstsmrf <145853084+jstsmrf@users.noreply.github.com>
2024-05-29 22:13:37 +0800

    deploy

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from 'cors';
 import mongoose from 'mongoose';
+import dotenv from "dotenv";
 
 import { userRouter } from './routes/users.js';
 import { MoviesRouter } from './routes/movies.js';
@@ -12,13 +13,15 @@ app.use(cors());
 
 app.use("/auth", userRouter);
 app.use("/movies", MoviesRouter);
+dotenv.config();
 
-mongoose.connect(
-    "mongodb+srv://18103864:CwiDJc3eHBR2b54L@3202finalprojectvercel.n9oigii.mongodb.net/"
-).then(()=> {
+const PORT = process.env.WEB_APP_API_PORT;
+
+mongoose.connect(process.env.MONGO_DB_CONNECTION)
+.then(()=> {
     console.log("CONNECTED TO DATABASE");
-    app.listen(3001, () => {
-        console.log(`SERVER STARTED RUNNING ON GIVEN PORT`);
+    app.listen(PORT, () => {
+        console.log(`SERVER STARTED RUNNING ON GIVEN PORT: ${PORT}`);
     })
 })
 .catch((err) => console.log(err))
